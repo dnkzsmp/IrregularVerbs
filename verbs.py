831456364af1a093fe047eac915827af3fa02242
Danil Kozlov <danilkozlov1003@gmail.com>
2020-04-03 19:03:01 +0700

    Rechanged function verbs, added new functions

--- verbs.py
+++ verbs.py
@@ -1,10 +1,7 @@
 import random
 
+
 def verbs():
-    global rus
-    global inf
-    global pasts
-    global pastp
     with open('verbs.txt', encoding='utf-8') as f:
         strokes = f.readlines()
     lines = [line.strip() for line in strokes]
@@ -13,3 +10,39 @@ def verbs():
     inf = words[1]
     pasts = words[2]
     pastp = words[3]
+    return rus, inf, pasts, pastp
+
+
+def correct_all(wrong, correct):
+    wrong += 0
+    correct += 3
+    return wrong, correct
+
+
+def correct_one(wrong, correct):
+    wrong += 2
+    correct += 1
+    print(wrong)
+    return wrong, correct
+
+
+def correct_two(wrong, correct):
+    wrong += 1
+    correct += 2
+    return wrong, correct
+
+
+def wrong_all(wrong, correct):
+    wrong += 3
+    correct += 0
+    return wrong, correct
+
+
+def counter(ch):
+    count = int(ch)
+    return count
+
+
+def Gapper(gap):
+    gap += 1
+    return gap
