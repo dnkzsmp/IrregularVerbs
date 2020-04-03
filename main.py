b955c170ebe8c9d8e43c642926b7e9239821c8c9
Danil Kozlov <danilkozlov1003@gmail.com>
2020-04-03 19:02:07 +0700

    Import verbs to StartMenu.py

--- main.py
+++ main.py
@@ -2,19 +2,16 @@ from StartMenu import *
 from tkinter import *
 
 
-
 def main():
     global app1
-    global count
-    count = 0
+    global app2
     root = Tk()
     app1 = StartMenu(root)
     root.mainloop()
+    window = Tk()
+    app2 = Graphic(window)
+    window.mainloop()
 
 
 if __name__ == '__main__':
-    rus = ''
-    inf = ''
-    pasts = ''
-    pastp = ''
     main()
