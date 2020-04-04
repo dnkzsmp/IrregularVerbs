0c70b497e09f778137e9122800864be76cc026df
KiryaDtlv <dyatlov.kiryuha@gmail.com>
2020-04-04 09:35:31 +0700

    Add StartMenu(root)

--- main.py
+++ main.py
@@ -2,19 +2,17 @@ from StartMenu import *
 from tkinter import *
 
 
-
 def main():
-    global app1
-    global count
-    count = 0
     root = Tk()
-    app1 = StartMenu(root)
+    StartMenu(root)
     root.mainloop()
+    window = Tk()
+    Graphic(window)
+    window.mainloop()
+    result = Tk()
+    Result(result)
+    result.mainloop()
 
 
 if __name__ == '__main__':
-    rus = ''
-    inf = ''
-    pasts = ''
-    pastp = ''
     main()
